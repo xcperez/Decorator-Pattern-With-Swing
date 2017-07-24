129954aab72e42b063701b9a1d70fe5501704a0b
ncc <ncc>
2017-07-24 14:42:08 -0700

    Created Frame

--- CoffeeShop/src/CoffeeShopGUI.java
+++ CoffeeShop/src/CoffeeShopGUI.java
@@ -7,7 +7,11 @@ public class CoffeeShopGUI {
 	public static void main(String[]args)
 	{
 		
-		
+		JFrame frame = new JFrame("Coffee Shop");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
 }
